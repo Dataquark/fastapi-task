c39f26da2ee895fb932c25574c45ad6547839e0c
Bubitrek <25078586+Dataquark@users.noreply.github.com>
2023-04-07 13:43:14 +0100

    Created temp/keys directory

--- app/bq/queries/queries.py
+++ app/bq/queries/queries.py
@@ -16,7 +16,7 @@ class QUERIES:
             procedure_dat as dates,
             COUNT(person_id) as person_count
         FROM `bigquery-public-data.cms_synthetic_patient_data_omop.procedure_occurrence`
-        WHERE procedure_dat >= (SELECT max_date - @n_date FROM cte)
+        WHERE procedure_dat > (SELECT max_date - @n_date FROM cte)
         GROUP BY dates
         ORDER BY dates DESC;        
         """
